1f671f386708a7db73f62eebd8a0f5d7d2eaed40
Arina <ariangerris@gmail.com>
2023-07-04 16:34:11 +0400

    lazy, suspense

--- 01/first-app/src/App.js
+++ 01/first-app/src/App.js
@@ -1,46 +1,50 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Feed from "./components/Feed/Feed";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import FriendsContainer from "./components/Friends/FriendsContainer";
 import NavbarContainer from "./components/Navbar/NavbarContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import { Provider, connect } from "react-redux";
 import { initAppThunk } from "./redux/app-reducer";
 import Preloader from "./components/common/preloader/preloader";
 import store from "./redux/reduxStore";
-
+const DialogsContainer = lazy(() =>
+  import("./components/Dialogs/DialogsContainer")
+);
+const ProfileContainer = lazy(() =>
+  import("./components/Profile/ProfileContainer")
+);
 class App extends React.Component {
   componentDidMount() {
     this.props.initAppThunk();
   }
   render() {
-    if (!this.props.initialized) return <Preloader />;
     return (
       <>
         <HeaderContainer />
         <div className="wrapper">
           <NavbarContainer />
           <div className="routes">
-            <Routes>
-              <Route
-                path="/profile/:profileId?"
-                element={<ProfileContainer />}
-              />
-              <Route path="/dialogs" element={<DialogsContainer />} />
-              <Route path="/feed" element={<Feed />} />
-              <Route path="/music" element={<Music />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/friends" element={<FriendsContainer />} />
-              <Route path="/users" element={<UsersContainer />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
+            <Suspense fallback={<Preloader />}>
+              <Routes>
+                <Route
+                  path="/profile/:profileId?"
+                  element={<ProfileContainer />}
+                />
+                <Route path="/dialogs" element={<DialogsContainer />} />
+                <Route path="/feed" element={<Feed />} />
+                <Route path="/music" element={<Music />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route path="/friends" element={<FriendsContainer />} />
+                <Route path="/users" element={<UsersContainer />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </Suspense>
           </div>
         </div>
       </>
@@ -52,7 +56,7 @@ const mapStateToProps = (state) => ({
   initialized: state.app.initialized,
 });
 const AppContainer = connect(mapStateToProps, { initAppThunk })(App);
-const ReactApp = (props) => {
+const ReactApp = () => {
   return (
     <BrowserRouter>
       <Provider store={store}>
